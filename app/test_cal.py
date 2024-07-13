42d911a6c6ca524b67bf868a4ea983791a6804d0
gilanggp <gigih136@gmail.com>
2024-07-13 05:19:54 +0000

    fix test code

--- app/test_cal.py
+++ app/test_cal.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_cal_func():
     response = client.get("/cal/12+3")
     assert response.status_code == 200
-    assert response.json() == {"comand": "12+3", "result", 15.0}
+    assert response.json() == {"comand": "12+3", "result": 15.0}
